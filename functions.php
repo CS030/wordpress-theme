5e3050b9e15272343090a43231d95fd79deda219
Jeroen Wellner <jeroen@wllnr.nl>
2017-12-06 15:22:09 +0100

    dequeueu scripts and css

--- functions.php
+++ functions.php
@@ -262,3 +262,74 @@ add_action( 'init', 'login_checked_remember_me' );
 function rememberme_checked() {
 	echo "<script>document.getElementById('rememberme').checked = true;</script>";
 }
+
+/**
+ * Supersaas filter
+ */
+
+add_filter( 'do_shortcode_tag','supersaas_format_output', 10, 3);
+
+function supersaas_format_output($output, $tag, $attr){
+	if('supersaas' != $tag)
+		return $output;
+
+	$output = str_replace("type=\"submit\"", "type=\"submit\" class=\"uk-button uk-button-primary\"", $output);
+	return $output;
+}
+
+/**
+ * WooCommerce
+ */
+
+ // remove woocommerce scripts on unnecessary pages
+function woocommerce_de_script() {
+	if (function_exists( 'is_woocommerce' )) {
+		if (!is_woocommerce() && !is_cart() && !is_checkout() && !is_account_page() ) { // if we're not on a Woocommerce page, dequeue all of these scripts
+			wp_dequeue_script('wc-add-to-cart');
+			wp_dequeue_script('jquery-blockui');
+			wp_dequeue_script('jquery-placeholder');
+			wp_dequeue_script('woocommerce');
+			wp_dequeue_script('jquery-cookie');
+			wp_dequeue_script('wc-cart-fragments');
+		}
+	}
+}
+add_action( 'wp_print_scripts', 'woocommerce_de_script', 100 );
+add_action( 'wp_enqueue_scripts', 'remove_woocommerce_generator', 99 );
+function remove_woocommerce_generator() {
+	if (function_exists( 'is_woocommerce' )) {
+		if (!is_woocommerce()) { // if we're not on a woo page, remove the generator tag
+			remove_action( 'wp_head', array( $GLOBALS['woocommerce'], 'generator' ) );
+		}
+	}
+}
+
+// remove woocommerce styles, then add woo styles back in on woo-related pages
+function child_manage_woocommerce_css(){
+	if (function_exists( 'is_woocommerce' )) {
+		if (!is_woocommerce()) { // this adds the styles back on woocommerce pages. If you're using a custom script, you could remove these and enter in the path to your own CSS file (if different from your basic style.css file)
+			wp_dequeue_style('woocommerce-layout');
+			wp_dequeue_style('woocommerce-smallscreen');
+			wp_dequeue_style('woocommerce-general');
+		}
+	}
+}
+add_action( 'wp_enqueue_scripts', 'child_manage_woocommerce_css' );
+
+function dequeue_buddypress() {
+	if (!is_admin()) {
+		wp_dequeue_style('bp-legacy-css');
+		wp_deregister_script('bp-jquery-query');
+		wp_deregister_script('bp-confirm');
+	}
+}
+add_action('wp_enqueue_scripts', 'dequeue_buddypress');
+
+function dequeue_mailpoet() {
+	if (!is_admin()) {
+		wp_dequeue_style('mailpoet_public');
+		wp_deregister_script('mailpoet_vendor');
+		wp_deregister_script('mailpoet_public');
+	}
+}
+add_action('wp_enqueue_scripts', 'dequeue_mailpoet');
