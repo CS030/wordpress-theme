34f97376308caa0932dddf9d8ce234e374e0ae88
Jeroen Wellner <jeroen@wllnr.nl>
2018-08-18 22:01:58 +0200

    Build custom SSO API for trainer module

--- includes/custom-api.php
+++ includes/custom-api.php
@@ -1,19 +1,55 @@
 <?php
-add_filter('jwt_auth_token_before_dispatch', 'add_user_data', 10, 2);
+/**
+ * Trainer module SSO
+ *
+ *
+ */
+add_action('parse_request', 'trainers_redirect');
+add_action('parse_request', 'trainers_sso');
 
-function add_user_data($data, $user)
-{
+function trainers_redirect() {
+   if($_SERVER["REQUEST_URI"] == '/trainers') {
+			$token = wp_get_session_token();
+			$user_id = get_current_user_id();
+			if($user_id) {
+				header("Location: http://trainers.cs030.nl/login.php?user_id=".$user_id."&token=".$token);
+			} else {
+				wp_redirect('/wp-login.php?redirect_to=/trainers');
+			}
+			exit();
+   }
+}
 
-		$ua_trainer = rua_get_level_by_name('trainer');
+function trainers_sso() {
+	if(strpos($_SERVER["REQUEST_URI"], '/trainers/sso') === 0) {
+		$manager = WP_Session_Tokens::get_instance( $_GET['user_id'] );
+		if ($manager->verify( $_GET['token'] )) {
+			$user = get_userdata($_GET['user_id']);
 
-		$is_trainer = $ua_trainer ? rua_has_user_level($user->ID, $ua_trainer->ID) : false;
+			$ua_trainer = rua_get_level_by_name('trainer');
+			$is_trainer = $ua_trainer ? rua_has_user_level($user->ID, $ua_trainer->ID) : false;
 
-    return $data = array(
-      'token'    => $data['token'],
-			'name' => $user->display_name,
-			'email' => $user->user_email,
-			'id' => $user->ID,
-			'joomla_id' => $user->supersaas_id,
-			'is_trainer' => $is_trainer,
-    );
+			$data = array();
+			$data["id"] = $user->id;
+			$data["name"] = $user->display_name;
+			$data["email"] = $user->user_email;
+			$data["joomla_id"] = $user->supersaas_id;
+			$data["is_trainer"] = $is_trainer;
+
+			$message = array();
+			$message["success"] = true;
+			$message["data"] = $data;
+
+			header('Content-Type: application/json');
+			echo json_encode($message);
+		} else {
+			$message = array();
+			$message["success"] = false;
+
+			header("HTTP/1.1 401 Unauthorized");
+			header('Content-Type: application/json');
+			echo json_encode($message);
+		}
+		exit();
+ }
 }
